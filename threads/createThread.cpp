c2b9e615b3defb054c97e7ace13baee036322b28
tianhang <476855396@qq.com>
2016-02-15 21:36:32 +0800

    multiple threads downloads

--- threads/createThread.cpp
+++ threads/createThread.cpp
@@ -10,6 +10,13 @@
 
 using namespace std;
 
+struct download_param{
+	char *filename;
+	char *p ;
+	int size;
+	int num; // the num of threads
+	int i;//thread i	
+}*param;
 
 int get_filesize(char *filename)
 {
@@ -48,16 +55,43 @@ void mmap_file(char *filename,char *mapaddress)
 
 void *thr_fn(void *arg)
 {
-	int i=4;
-	int *p = (int*)arg;
-	while(i--)
+	struct download_param *p = (struct download_param *)arg;
+	
+	char buf[1];
+	int b = p->size/p->num;
+	char * a = p->p;
+	char * r_start =  a+b*(p->i);
+	char * r_end;
+
+	cout<<"thread num:"<<p->i<<endl;
+//	cout<<"r_start:"<<r_start<<endl;
+
+	if(p->i == p->num-1)
 	{
-		cout<<p[i]<<endl;
+		r_end = a+ p->size-1;
 	}
+	else{
+		r_end = a + b*(p->i) + b-1;
+	}
+	
+
+//	cout<<"r_end:"<<r_end<<endl;
+	int fd2 = open(p->filename,O_RDWR|O_CREAT,0644);	
+
+	lseek(fd2,p->i*b,SEEK_SET);
+
+	for(;r_start <= r_end;r_start++)	
+	{
+		buf[0] = *r_start;
+		cout<<"thread:	"<<p->i<<"---->"<<"print:"<<buf[0]<<endl;
+		write(fd2,buf,1); 	
+	}
+	
+	close(fd2);
 	return (void *)1;
 }
 
-int main(int argc,int *argv[])
+int main(int arg,char *argv[])
 {
 	pthread_t tid;
 	void *tret;
@@ -84,9 +118,11 @@ int main(int argc,int *argv[])
 	}
 	close(fd);
 
-// start to write
+	// start to write
 
-	char filename2 [] = "demo12";
+	cout<<"p:"<<p<<endl;		
+
+	char filename2 [] = "demo13";
 	int fd2 = open(filename2,O_RDWR|O_CREAT,0644);	
 	//expand file
 	int size2 = get_filesize(filename2);
@@ -95,32 +131,40 @@ int main(int argc,int *argv[])
 	close(fd2);
 
 
-	fd2 = open(filename2,O_RDWR|O_CREAT,0644);	
 
-	char buf[1];
-	int l = size;
-	while(l--)
-	{
-		buf[0] = *p++;
-		write(fd2,buf,1); 	
-	}
-	
 	 size2 = get_filesize(filename2);
 	cout<<"size1:"<<size<<endl;
 	cout<<"size2:"<<size2<<endl;
 	close(fd2);
 
 	int num = 10;
+	int detachstate;	
+	pthread_t tid_arr[num]; 	
+	struct download_param parameter[num];
+
 	for(int i =0 ;i<num;i++)
 	{
 		//param: p(head address),size,num,i
-		int param_arr[4] = {(int*)p,2,3,4};
-		int err = pthread_create(&tid,NULL,thr_fn,param_arr);
+		parameter[i].filename = filename2;
+		parameter[i].p = p;
+		parameter[i].size = size;
+		parameter[i].num = num;
+		parameter[i].i = i;
+		param = &parameter[i];
+
+		pthread_attr_t attr;
+
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+		int err = pthread_create(&tid_arr[i],&attr,thr_fn,param);
+
+//		sleep(1);
+		printf("err:%d\n",err);
 	
 	}
 
 	sleep(2);
-	printf("err:%d\n",err);
+//	printf("err:%d\n",err);
 
 	return 0;		
 }
